3e6a32a46af81cacaeb14d267c1902e4775a096e
luc.jean <devnull@localhost>
2013-05-14 17:39:25 +0200

    use a setup.py

---
+++ README.rst
@@ -0,0 +1,28 @@
+sanza-crm
+===============================================
+* `Overview`_
+* `Quick start`_
+
+.. _Overview: #overviex
+.. _Quick start: #quick-start
+
+
+.. _overview:
+
+Overview
+------------------------------------
+
+sanza-crm moto is Organize > Target > Communicate. It helps you to work with your contacts in your day-to-day work
+
+.. _quick-start:
+
+Quick start
+-----------
+
+Python 2.6+, Django 1.4+ required
+
+
+License
+=======
+
+CeCCIL.
---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import ez_setup
+    ez_setup.use_setuptools()
+    from setuptools import setup, find_packages
+
+VERSION = __import__('sanza').__version__
+
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'sanza',
+    version = VERSION,
+    description = 'Contacts management',
+    packages = find_packages(),
+    include_package_data = True,
+    author = 'Luc Jean',
+    author_email = 'ljean@apidev.fr',
+    license = 'CeCILL',
+    zip_safe = False,
+    install_requires = [
+        #'coop-cms',
+    ],
+    long_description = open('README.rst').read(),
+    url = 'https://bitbucket.org/ljean/apidev-sanza',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Natural Language :: English',
+        'Natural Language :: French',
+        'Topic :: Office/Business',
+    ],
+)
+
