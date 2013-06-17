5d490f9ef1d225bb923c74fe35855ddebc4913cf
luc.jean <devnull@localhost>
2013-06-17 22:46:17 +0200

    Fix

--- sanza/Crm/coop_bar_cfg.py
+++ sanza/Crm/coop_bar_cfg.py
@@ -7,10 +7,11 @@ from coop_cms.coop_bar_cfg import can_edit_object
 @can_edit_object
 def doc_to_pdf(request, context):
     if request.user.is_authenticated() and request.user.is_staff:
-        object = context["object"]
-        if not context.get('edit_mode') and hasattr(object, 'get_pdf_url'):
-            return make_link(object.get_pdf_url(), _(u'Pdf'), 'fugue/document-pdf.png',
-                classes=['icon', 'alert_on_click'])
+        object = context.get("object", None)
+        if object:
+            if not context.get('edit_mode') and hasattr(object, 'get_pdf_url'):
+                return make_link(object.get_pdf_url(), _(u'Pdf'), 'fugue/document-pdf.png',
+                    classes=['icon', 'alert_on_click'])
 
 def load_commands(coop_bar):
     
