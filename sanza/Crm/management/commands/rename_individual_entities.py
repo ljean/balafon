d15d679080aa2d28ac26895fd6177929596b8540
Luc Jean <ljean@apidev.fr>
2013-07-04 22:30:27 +0200

    rename_individual_entity command

---
+++ sanza/Crm/management/commands/rename_individual_entities.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+from sanza.Crm import models
+from datetime import date
+from sanza.Crm.settings import ALLOW_SINGLE_CONTACT
+from django.conf import settings
+
+class Command(BaseCommand):
+    help = u"add a new credit of emailings"
+
+    def handle(self, *args, **options):
+        verbose = options.get('verbosity', 0)
+
+        if ALLOW_SINGLE_CONTACT:
+            print "this command is disabled if SANZA_ALLOW_SINGLE_CONTACT is set"
+            return
+        
+        individual_entity_id = getattr(settings, 'SANZA_INDIVIDUAL_ENTITY_ID', 1)
+        
+        for e in models.Entity.objects.get(id=individual_entity_id):
+            e.name = u"{0.lastname} {0.firstname}".format(e.default_contact).strip().upper()
+            e.save()
+        
+            if verbose:
+                print e.name
+        if verbose:
+            print models.Entity.objects.get(id=individual_entity_id).count(), "entities have been renamed"
+            
