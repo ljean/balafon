8dcdb218322c457a6acf49391199bac2c95ecc24
Luc Jean <ljean@apidev.fr>
2013-07-17 11:56:29 +0200

    clean emailing_utils and add a template_tag for debugging

--- sanza/Emailing/templatetags/emailing_utils.py
+++ sanza/Emailing/templatetags/emailing_utils.py
@@ -2,21 +2,25 @@
 
 from django import template
 register = template.Library()
-from djaloha.templatetags.djaloha_utils import DjalohaEditNode
-from Emailing.models import HtmlFragment
+#from djaloha.templatetags.djaloha_utils import DjalohaEditNode
+#from sanza.Emailing.models import HtmlFragment
+#
+#class HtmlFragmentEditNode(DjalohaEditNode):
+#    def render(self, context):
+#        context.dicts[0]['can_edit_template'] = True
+#        for (k, v) in self._lookup.items():
+#            new_v = v.strip('"').strip("'")
+#            if len(v)-2 == len(new_v):
+#                self._lookup[k] = new_v
+#            else:
+#                self._lookup[k] = context[v]
+#        return super(HtmlFragmentEditNode, self).render(context)
+#
+#@register.tag
+#def html_edit(parser, token):
+#    div_id = token.split_contents()[1]
+#    return HtmlFragmentEditNode(HtmlFragment, {'div_id': div_id}, 'content')
 
-class HtmlFragmentEditNode(DjalohaEditNode):
-    def render(self, context):
-        context.dicts[0]['can_edit_template'] = True
-        for (k, v) in self._lookup.items():
-            new_v = v.strip('"').strip("'")
-            if len(v)-2 == len(new_v):
-                self._lookup[k] = new_v
-            else:
-                self._lookup[k] = context[v]
-        return super(HtmlFragmentEditNode, self).render(context)
-
-@register.tag
-def html_edit(parser, token):
-    div_id = token.split_contents()[1]
-    return HtmlFragmentEditNode(HtmlFragment, {'div_id': div_id}, 'content')
+@register.filter
+def dir_debug(obj):
+    return unicode(dir(obj))
