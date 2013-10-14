fd6afcb9ae91908de93bcbcb43d822c164deef2b
Luc Jean <ljean@apidev.fr>
2013-10-14 11:54:42 +0200

    "Back to site" in menu

--- sanza/Crm/context_processors.py
+++ sanza/Crm/context_processors.py
@@ -3,9 +3,19 @@
 from sanza.Crm.models import EntityType
 from django.conf import settings 
 from sanza.Crm import settings as crm_settings
+from django.core.urlresolvers import reverse, NoReverseMatch
 
 def crm(request):
+    
+    site_url = None
+    if not settings.SANZA_AS_HOMEPAGE:
+        try:
+            site_url = reverse('homepage')
+        except NoReverseMatch:
+            pass
+    
     return {
+        'SANZA_SITE_URL': site_url,
         'SANZA_ALLOW_SINGLE_CONTACT': crm_settings.ALLOW_SINGLE_CONTACT,
         'SANZA_ENTITY_TYPES': EntityType.objects.all(),
         'SANZA_MULTI_USER': getattr(settings, 'SANZA_MULTI_USER', True),
