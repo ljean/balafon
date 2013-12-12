1611546d981efb9942f09b9b9a1911b725cfa5b7
Luc Jean <ljean@apidev.fr>
2013-12-12 16:37:04 +0100

    rename class

--- sanza/Crm/translation.py
+++ sanza/Crm/translation.py
@@ -3,7 +3,7 @@
 from modeltranslation.translator import translator, TranslationOptions
 from models import EntityType
 
-class ArticleTranslationOptions(TranslationOptions):
+class EntityTypeTranslationOptions(TranslationOptions):
     fields = ('name',)
 
-translator.register(EntityType, ArticleTranslationOptions)
+translator.register(EntityType, EntityTypeTranslationOptions)
