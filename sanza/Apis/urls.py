0fa1fc6ee1efa4490dccac6312d0cdf589326025
Luc Jean <ljean@apidev.fr>
2013-12-24 16:57:48 +0100

    Fix import

--- sanza/Apis/urls.py
+++ sanza/Apis/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls import patterns, url, include
 
 #from rest_framework import routers
-from sanza.Apis import views
+#from sanza.Apis import views
 
 #router = routers.DefaultRouter()
 #router.register(r'crm-groups', views.GroupViewSet)
