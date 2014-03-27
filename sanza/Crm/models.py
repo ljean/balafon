33cc3e2289b18345a0c735caec880292f5590230
Luc Jean <ljean@apidev.fr>
2014-03-27 10:11:16 +0100

    Fix unicode error

--- sanza/Crm/models.py
+++ sanza/Crm/models.py
@@ -595,7 +595,7 @@ class Contact(TimeStampedModel):
         super(Contact, self).save(*args, **kwargs)
         if not self.uuid:
             ln = unicodedata.normalize('NFKD', unicode(self.fullname)).encode("utf8",'ignore')
-            name = '{0}-contact-{1}-{2}-{3}'.format(project_settings.SECRET_KEY, self.id, ln, self.email)
+            name = u'{0}-contact-{1}-{2}-{3}'.format(project_settings.SECRET_KEY, self.id, ln, self.email)
             self.uuid = uuid.uuid5(uuid.NAMESPACE_URL, name)
             return super(Contact, self).save()
         
