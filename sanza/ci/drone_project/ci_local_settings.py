3502ed64071ee744c63f1c87c2a990c3f3fee599
Luc Jean <ljean@apidev.fr>
2015-04-16 13:58:24 +0200

    Fix drone build

--- sanza/ci/drone_project/ci_local_settings.py
+++ sanza/ci/drone_project/ci_local_settings.py
@@ -2,22 +2,11 @@
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'test.sqlite3',
-#         'USER': '',
-#         'PASSWORD': '',
-#         'HOST': '',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'sanza_ci',
+#         'USER': 'postgres',
+#         'PASSWORD': 'lynxlynx',
+#         'HOST': 'localhost',
 #         'PORT': '',
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sanza_ci',
-        'USER': 'postgres',
-        'PASSWORD': 'lynxlynx',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
