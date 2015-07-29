3c85f79e7de41357719e8eb3947b87b7911b764e
Luc Jean <ljean@apidev.fr>
2015-07-29 15:07:57 +0200

    utilities metods

--- sanza/Crm/models.py
+++ sanza/Crm/models.py
@@ -335,6 +335,19 @@ class Entity(TimeStampedModel):
         """logo thumbnail"""
         return sorl_thumbnail.backend.get_thumbnail(self.logo.file, "128x128", crop='center')
 
+    def set_custom_field(self, field_name, value):
+        """set the value of a custom field"""
+        field = CustomField.objects.get_or_create(model=CustomField.MODEL_ENTITY, name=field_name)[0]
+        field_value = EntityCustomFieldValue.objects.get_or_create(custom_field=field, contact=self)[0]
+        field_value.value = value
+        field_value.save()
+
+    def add_to_group(self, group_name):
+        """add to group"""
+        group = Group.objects.get_or_create(group_name)[0]
+        group.entities.add(self)
+        group.save()
+
     def get_custom_fields(self):
         """additional fields"""
         return CustomField.objects.filter(model=CustomField.MODEL_ENTITY)
@@ -706,6 +719,19 @@ class Contact(TimeStampedModel):
             return _(u"{1}{0.firstname}{0.lastname}").format(self, title)
         
         return _(u"{1}{0.firstname} {0.lastname}").format(self, title)
+
+    def set_custom_field(self, field_name, value):
+        """set the value of a custom field"""
+        field = CustomField.objects.get_or_create(model=CustomField.MODEL_CONTACT, name=field_name)[0]
+        field_value = ContactCustomFieldValue.objects.get_or_create(custom_field=field, contact=self)[0]
+        field_value.value = value
+        field_value.save()
+
+    def add_to_group(self, group_name):
+        """add to group"""
+        group = Group.objects.get_or_create(group_name)[0]
+        group.contacts.add(self)
+        group.save()
 
     def save(self, *args, **kwargs):
         """save"""
