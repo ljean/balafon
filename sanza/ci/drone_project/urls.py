3718b51dfd41ebb7acd3ae0567a6e9191f711152
Luc Jean <ljean@apidev.fr>
2016-01-06 18:36:44 +0100

    Fix urls in drone-ci project

--- sanza/ci/drone_project/urls.py
+++ sanza/ci/drone_project/urls.py
@@ -1,20 +1,8 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, include, url
 
 from sanza.urls import urlpatterns as sanza_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^localeurl/', include('localeurl.urls')),
-    (r'^captcha/', include('captcha.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-)
-
-urlpatterns += sanza_urlpatterns
+urlpatterns = sanza_urlpatterns
