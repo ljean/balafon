00d97ac7884eb44fe2ab5818e9d14b60fe297043
Luc Jean <ljean@apidev.fr>
2016-01-18 17:03:10 +0100

    Fix install requirements

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         'django-cors-headers',
     ],
     dependency_links=[
-        'git+https://github.com/ljean/coop_cms.git@3dda8592da19cfb5c79d1badf9e320ac211cb2fe#egg=apidev_coop_cms-dev',
+        'git+https://github.com/ljean/coop_cms.git@dfed5fb535ff4986177f37b42f1d3e2568015a7a#egg=apidev_coop_cms-dev',
     ],
     long_description=open('README.rst').read(),
     url='https://bitbucket.org/ljean/apidev-sanza',
