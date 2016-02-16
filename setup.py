fc87a096087198da259d439fe241ccedd9242346
Luc Jean <ljean@apidev.fr>
2016-02-16 19:15:21 +0100

    Force usage of coop_ms 1.1.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     install_requires=[
         'django >= 1.6, <1.10',
         'djangorestframework',
-        'apidev-coop_cms >= 1.1.0',
+        'apidev-coop_cms >= 1.1.1',
         'beautifulsoup4',
         'BeautifulSoup',
         'django-wkhtmltopdf',
