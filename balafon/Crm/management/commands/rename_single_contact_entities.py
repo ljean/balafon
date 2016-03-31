c6e2182051a08b0a9cc65779200e6e55fafafe32
Luc Jean <ljean@apidev.fr>
2016-03-31 16:16:57 +0200

    close #3: Add command "rename_single_contact_entities"  to change the name of single contact entities. The name of a single contact entity should be "lastname firstname" of the contact and should not contain civility to match with list of contacts filtering

---
+++ balafon/Crm/management/commands/rename_single_contact_entities.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from balafon.Crm import models
+from balafon.Crm.settings import ALLOW_SINGLE_CONTACT
+
+
+class Command(BaseCommand):
+    help = u"add a new credit of emailings"
+    use_argparse = False
+
+    def handle(self, *args, **options):
+        verbose = options.get('verbosity', 0)
+
+        if not ALLOW_SINGLE_CONTACT:
+            print "this command is disabled if BALAFON_ALLOW_SINGLE_CONTACT is not set"
+            return
+        
+        for entity in models.Entity.objects.filter(is_single_contact=True):
+            entity.name = u"{0.lastname} {0.firstname}".format(entity.default_contact).strip().lower()
+            entity.save()
+        
+            if verbose:
+                print entity.name
+        print models.Entity.objects.filter(is_single_contact=True).count(), "entities have been renamed"
