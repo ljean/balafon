919d2f5978a1525416d0c10fbbaa6b97bafbb5e5
Luc Jean <ljean@apidev.fr>
2016-05-25 00:41:06 +0200

    Fix Users app verbose name

--- balafon/Users/apps.py
+++ balafon/Users/apps.py
@@ -4,12 +4,12 @@ app configuration
 """
 
 from django import VERSION
+from django.utils.translation import ugettext_lazy as _
+
 
 if VERSION > (1, 7, 0):
     from django.apps import AppConfig
 
     class BalafonAppConfig(AppConfig):
         name = 'balafon.Users'
-        verbose_name = "Balafon Users"
-
-default_app_config = 'balafon.Users.apps.BalafonAppConfig'
+        verbose_name = _(u"Balafon Users")
