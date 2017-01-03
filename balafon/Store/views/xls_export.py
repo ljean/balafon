463408b84e870e4d676f0ceae2f57dfb68ebdb66
Luc Jean <ljean@apidev.fr>
2017-01-03 09:33:29 +0100

    Improve export of the store

--- balafon/Store/views/xls_export.py
+++ balafon/Store/views/xls_export.py
@@ -9,8 +9,23 @@ from balafon.generic import XlsExportView
 from balafon.Store.models import StoreItem
 
 
-class StockXlsView(XlsExportView):
+class XlsBaseView(XlsExportView):
+
+    def get_header_style(self):
+        """style of header"""
+        style = xlwt.XFStyle()
+        style.pattern = xlwt.Pattern()
+        style.pattern.pattern = xlwt.Pattern.SOLID_PATTERN
+        style.pattern.pattern_fore_colour = 22
+        return style
+
+
+class StockXlsView(XlsBaseView):
     doc_name = 'export-stock.xls'
+
+    def get_store_items(self):
+        """returns objects to export"""
+        return StoreItem.objects.all().order_by('category__name', 'name')
 
     def do_fill_workbook(self, workbook):
         """export to excel all articles tin stock"""
@@ -19,48 +34,38 @@ class StockXlsView(XlsExportView):
 
         line = 0
 
-        for line, item in enumerate(StoreItem.objects.filter(stock_count__isnull=False).order_by('name')):
-            sheet.write(line, 0, item.name)
-            sheet.write(line, 1, unicode(item.category) if item.category else '')
-            sheet.write(line, 2, item.purchase_price if item.purchase_price else 0)
-            sheet.write(line, 3, item.stock_count)
-            sheet.write(line, 4, item.stock_threshold)
-            sheet.write(line, 5, xlwt.Formula('C{0}*D{0})'.format(line+1)))
+        columns = [
+            _(u'Id'), _(u'Name'), _(u'Category'), _(u'Purchase price'), _(u"Stock count"), _(u'Stock threshold'),
+            _(u'Value'),
+        ]
 
-        sheet.write(line+2, 5, xlwt.Formula('SUM(F1:F{0})'.format(line+1)))
+        for col, label in enumerate(columns):
+            self.write_cell(sheet, 0, col, label, style=self.get_header_style())
+
+        for line, item in enumerate(self.get_store_items()):
+            sheet.write(line + 1, 0, item.id)
+            sheet.write(line + 1, 1, item.name)
+            sheet.write(line + 1, 2, u'{0}'.format(item.category) if item.category else '')
+            sheet.write(line + 1, 3, item.purchase_price if item.purchase_price else 0)
+            sheet.write(line + 1, 4, '' if item.stock_count is None else item.stock_count)
+            sheet.write(line + 1, 5, '' if item.stock_count is None else item.stock_threshold)
+            sheet.write(line + 1, 6, xlwt.Formula('D{0}*E{0})'.format(line + 2)))
+
+        sheet.write(line + 3, 6, xlwt.Formula('SUM(G1:G{0})'.format(line + 2)))
 
 
-class StockAlertXlsView(XlsExportView):
+class StockAlertXlsView(StockXlsView):
     doc_name = 'export-stock-alert.xls'
 
-    def do_fill_workbook(self, workbook):
-        """export to excel all articles that must be purchased"""
-
-        sheet = workbook.add_sheet(_(u"Stock"))
-
-        line = 0
-        for item in StoreItem.objects.filter(stock_count__isnull=False).order_by('name'):
+    def get_store_items(self):
+        """returns objects to export"""
+        for item in StoreItem.objects.filter(stock_count__isnull=False):
             if item.has_stock_threshold_alert():
-                sheet.write(line, 0, item.name)
-                sheet.write(line, 1, unicode(item.category) if item.category else '')
-                sheet.write(line, 2, item.purchase_price if item.purchase_price else 0)
-                sheet.write(line, 3, item.stock_count)
-                sheet.write(line, 4, item.stock_threshold)
-                sheet.write(line, 5, xlwt.Formula('C{0}*D{0})'.format(line+1)))
-                line += 1
-
-        sheet.write(line+2, 5, xlwt.Formula('SUM(F1:F{0})'.format(line+1)))
+                yield item
 
 
-class StoreXlsCatalogueView(XlsExportView):
+class StoreXlsCatalogueView(XlsBaseView):
     doc_name = 'catalogue.xls'
-
-    def get_header_style(self):
-        style = xlwt.XFStyle()
-        style.pattern = xlwt.Pattern()
-        style.pattern.pattern = xlwt.Pattern.SOLID_PATTERN
-        style.pattern.pattern_fore_colour = 22
-        return style
 
     def do_fill_workbook(self, workbook):
         """implement it in base class"""
@@ -77,7 +82,8 @@ class StoreXlsCatalogueView(XlsExportView):
             queryset = queryset.filter(category=category_id)
 
         columns = [
-            _(u'Name'), _(u'Category'), _(u'Brand'), _(u"VAT inclusive price"), _(u'Reference'), _(u'certificates'),
+            _(u'Name'), _(u'Category'), _(u'Brand'), _(u"VAT inclusive price"), _(u'Reference'),
+            _(u'certificates'),
         ]
 
         for col, label in enumerate(columns):
