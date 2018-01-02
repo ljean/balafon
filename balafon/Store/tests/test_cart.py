75d0a31fd2df77199f2e7be7af8bc62f0d8d3756
Luc Jean <ljean@apidev.fr>
2018-01-02 14:24:36 +0100

    test cart_callback

--- balafon/Store/tests/test_cart.py
+++ balafon/Store/tests/test_cart.py
@@ -27,6 +27,14 @@ def custom_confirmation_email_subject(profile, action):
     return u'Hello {0}'.format(u''.join([contact.firstname for contact in action.contacts.all()]))
 
 
+GLOBAL_COUNTER = 0
+
+
+def cart_processed_callback(cart):
+    global GLOBAL_COUNTER
+    GLOBAL_COUNTER += 1
+
+
 class BaseTestCase(APITestCase):
     """Base class for test cases"""
 
@@ -702,3 +710,77 @@ class CartTest(BaseTestCase):
         self.assertEqual(len(mail.outbox), 2)
         self.assertEqual(mail.outbox[0].to, [profile.contact.email])
         self.assertEqual(mail.outbox[1].to, [settings.BALAFON_NOTIFICATION_EMAIL])
+
+    @override_settings(BALAFON_CART_PROCESSED_CALLBACK='balafon.Store.tests.test_cart.cart_processed_callback')
+    def test_post_cart_callback(self):
+        """It should create a new sale and action"""
+
+        global GLOBAL_COUNTER
+        GLOBAL_COUNTER = 0
+
+        # Create contact for the user
+        profile = create_profile_contact(self.user)
+        contact = profile.contact
+
+        store_item1 = mommy.make(models.StoreItem)
+        store_item2 = mommy.make(models.StoreItem)
+
+        delivery_point = mommy.make(models.DeliveryPoint)
+
+        data = {
+            'items': [
+                {'id': store_item1.id, 'quantity': 2},
+                {'id': store_item2.id, 'quantity': 1},
+            ],
+            'delivery_point': delivery_point.id,
+            'purchase_datetime': datetime(2015, 7, 23, 12, 0)
+        }
+
+        url = reverse('store_post_cart')
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(response.data['ok'], True)
+        self.assertEqual(len(response.data['warnings']), 0)
+        self.assertEqual(response.data['deliveryDate'], data['purchase_datetime'])
+        self.assertEqual(response.data['deliveryPlace'], delivery_point.name)
+
+        action_type = ActionType.objects.get(name=get_cart_type_name())
+
+        action_queryset = Action.objects.filter(type=action_type)
+        self.assertEqual(action_queryset.count(), 1)
+        action = action_queryset[0]
+
+        self.assertEqual(list(action.contacts.all()), [contact])
+        self.assertEqual(action.subject, '')
+        self.assertEqual(action.detail, '')
+        self.assertEqual(action.planned_date, data['purchase_datetime'])
+
+        self.assertEqual(action.sale.analysis_code, models.SaleAnalysisCode.objects.get(name='Internet'))
+
+        self.assertEqual(action.sale.saleitem_set.count(), 2)
+        self.assertEqual(action.sale.saleitem_set.all()[0].item, store_item1)
+        self.assertEqual(action.sale.saleitem_set.all()[0].text, store_item1.name)
+        self.assertEqual(action.sale.saleitem_set.all()[0].unit_price(), store_item1.pre_tax_price)
+        self.assertEqual(action.sale.saleitem_set.all()[0].vat_rate, store_item1.vat_rate)
+        self.assertEqual(action.sale.saleitem_set.all()[0].quantity, 2)
+
+        self.assertEqual(action.sale.saleitem_set.all()[1].item, store_item2)
+        self.assertEqual(action.sale.saleitem_set.all()[1].text, store_item2.name)
+        self.assertEqual(action.sale.saleitem_set.all()[1].unit_price(), store_item2.pre_tax_price)
+        self.assertEqual(action.sale.saleitem_set.all()[1].vat_rate, store_item2.vat_rate)
+        self.assertEqual(action.sale.saleitem_set.all()[1].quantity, 1)
+
+        self.assertEqual(len(mail.outbox), 2)
+        self.assertEqual(mail.outbox[0].to, [profile.contact.email])
+        self.assertEqual(mail.outbox[1].to, [settings.BALAFON_NOTIFICATION_EMAIL])
+
+        store_item1 = models.StoreItem.objects.get(id=store_item1.id)
+        self.assertEqual(store_item1.stock_count, None)
+
+        store_item2 = models.StoreItem.objects.get(id=store_item2.id)
+        self.assertEqual(store_item2.stock_count, None)
+
+        # Make sure the calback has been called
+        self.assertEqual(GLOBAL_COUNTER, 1)
