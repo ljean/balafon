d86fc6ec3f9f0b4c9c38239a88c179f29fe6737e
Luc Jean <ljean@apidev.fr>
2019-07-03 14:05:42 +0200

    Fix ManyToMany admin widget

--- balafon/widgets.py
+++ balafon/widgets.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 from django.contrib.admin.widgets import ManyToManyRawIdWidget
 from django.core.urlresolvers import reverse
 from django.utils.encoding import smart_text
-from django.utils.html import escape
+from django.utils.html import escape, mark_safe
 
 import floppyforms.__future__ as forms
 
@@ -15,31 +15,30 @@ class VerboseManyToManyRawIdWidget(ManyToManyRawIdWidget):
     A Widget for displaying ManyToMany ids in the "raw_id" interface rather than
     in a <select multiple> box. Display user-friendly value like the ForeignKeyRawId widget
     """
+
     def __init__(self, rel, attrs=None, *args, **kwargs):
-        super(VerboseManyToManyRawIdWidget, self).__init__(rel, attrs, *args, **kwargs)
-        
-    def label_for_value(self, value):
-        values = value.split(',')
+        super().__init__(rel, attrs, *args, **kwargs)
+
+    def label_and_url_for_value(self, value):
+        values = value
         str_values = []
         key = self.rel.get_related_field().name
         app_label = self.rel.to._meta.app_label
         class_name = self.rel.to._meta.object_name.lower()
-        for v in values:
+        for elt in values:
             try:
-                obj = self.rel.to._default_manager.using(self.db).get(**{key: v})
+                obj = self.rel.to._default_manager.using(self.db).get(**{key: elt})
                 url = reverse('admin:{0}_{1}_change'.format(app_label, class_name), args=[obj.id])
                 label = escape(smart_text(obj))
-                
-                x = '<a href="{0}" {1}>{2}</a>'.format(
+                link = '<a href="{0}" {1}>{2}</a>'.format(
                     url,
                     'onclick="return showAddAnotherPopup(this);" target="_blank"',
                     label
                 )
-                
-                str_values += [x]
+                str_values += [link]
             except self.rel.to.DoesNotExist:
                 str_values += ['???']
-        return '&nbsp;<strong>{0}</strong>'.format(',&nbsp;'.join(str_values))
+        return mark_safe('&nbsp;<strong>{0}</strong>'.format(',&nbsp;'.join(str_values))), ''
 
 
 class CalcHiddenInput(forms.HiddenInput):
