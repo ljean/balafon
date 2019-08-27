9c440d633982c0b1e321179f7d67228c1f16e8e8
Benjamin Cherpas <benjamin.cherpas@apidev.fr>
2019-08-27 14:36:49 +0200

    Several changes on bootstrap

--- balafon/Crm/search_forms.py
+++ balafon/Crm/search_forms.py
@@ -237,7 +237,8 @@ class ZoneSearchForm(SearchFieldForm):
         return forms.SelectMultiple(attrs={
             'class': "chosen-select",
             'data-placeholder': self.label,
-            'style': "width: 100%", 
+            'style': "width: 100%",
+            'width': '100%',
         })
     
     def get_queryset(self, queryset):
@@ -692,7 +693,8 @@ class MultiGroupSearchForm(SearchFieldForm):
         return forms.SelectMultiple(attrs={
             'class': "chosen-select",
             'data-placeholder': _('Select Group names'),
-            'style': "width: 100%", 
+            'style': "width: 100%",
+            'width': "100%",
         })
 
 
