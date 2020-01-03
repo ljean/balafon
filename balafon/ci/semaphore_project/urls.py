b4d4158c672b713d37d2db590419b73b38696edc
Luc Jean <ljean@apidev.fr>
2020-01-03 14:44:34 +0100

    Fix import of admin urls

--- balafon/ci/semaphore_project/urls.py
+++ balafon/ci/semaphore_project/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 
 from coop_cms.settings import get_url_patterns
@@ -13,8 +13,7 @@ admin.autodiscover()
 localized_patterns = get_url_patterns()
 
 urlpatterns = localized_patterns(
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 )
 
 urlpatterns += balafon_urlpatterns
