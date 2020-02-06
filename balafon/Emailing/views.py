d5d28bf6d9ec368cc3c7a800108ebc1cae5be419
Luc Jean <ljean@apidev.fr>
2020-02-06 08:02:08 +0100

    pep8

--- balafon/Emailing/views.py
+++ balafon/Emailing/views.py
@@ -199,7 +199,7 @@ def view_link(request, link_uuid, contact_uuid):
         contact = Contact.objects.get(uuid=contact_uuid)
         link.visitors.add(contact)
 
-        #create action
+        # create action
         link_action = ActionType.objects.get_or_create(name=_('Link'))[0]
         action = Action.objects.create(
             subject=link.url, planned_date=now_rounded(),
@@ -262,7 +262,7 @@ def unregister_contact(request, emailing_id, contact_uuid):
                     }
                 )
             else:
-                pass #not valid : display with errors
+                pass  # not valid : display with errors
 
         else:
             return render(
