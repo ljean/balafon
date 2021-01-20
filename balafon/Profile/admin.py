8de758af4cd91a50968bb31f32093fae152c1262
Luc Jean <ljean@apidev.fr>
2021-01-20 17:56:46 +0100

    Amélioration admin ContactProfile

--- balafon/Profile/admin.py
+++ balafon/Profile/admin.py
@@ -1,17 +1,35 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _, ugettext
 
 from . import models
+from .utils import create_profile_contact
 
 
 class ContactProfileCustomFieldInline(admin.TabularInline):
     model = models.ContactProfileCustomField
 
 
+def finalize_user_profile(modeladmin, request, queryset):
+    for profile in queryset:
+        if profile.contact is None:
+            create_profile_contact(profile.user)
+            messages.success(request, ugettext('{0}: Contact created').format(profile.user))
+        else:
+            messages.warning(request, ugettext('{0}: Contact already existing').format(profile.user))
+
+
+finalize_user_profile.short_description = _('Create contact')
+
+
 @admin.register(models.ContactProfile)
 class ContactProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'contact']
+    raw_id_fields = ['user', 'contact', 'city']
     inlines = [ContactProfileCustomFieldInline]
+    actions = [finalize_user_profile]
 
 
 @admin.register(models.CategoryPermission)
