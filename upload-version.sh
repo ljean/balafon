49251841f0c7f0b0fac4eabc99eae633829519cc
ljean <ljean@apidev.fr>
2025-09-09 18:03:39 +0200

    upload version script

---
+++ upload-version.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# python -m pip install --upgrade setuptools wheel
+# pip install --upgrade twine
+python setup.py sdist bdist_wheel
+python -m twine upload dist/*
+rm -r dist
+rm -r build
